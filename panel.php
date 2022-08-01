50e4b0c58a54d85153148b355706e6126df72cce
febriansyahh <syahbanafebrian41@gmail.com>
2022-08-01 11:39:00 +0700

    up

--- panel.php
+++ panel.php
@@ -353,7 +353,7 @@ error_reporting(E_ALL ^ E_NOTICE);
           </div>
           <!-- Navbar links -->
           <ul class="navbar-nav align-items-center">
-            <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
               <a class="nav-link text-dark notification-bell unread dropdown-toggle " data-unread-notifications="true" href="#" role="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                 <svg class="icon icon-sm text-gray-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
@@ -365,7 +365,6 @@ error_reporting(E_ALL ^ E_NOTICE);
                   <a href="#" class="list-group-item list-group-item-action border-bottom">
                     <div class="row align-items-center">
                       <div class="col-auto">
-                        <!-- Avatar -->
                         <img alt="Image placeholder" src="ass/assets/img/team/profile-picture-1.jpg" class="avatar-md rounded">
                       </div>
                       <div class="col ps-0 ms-2">
@@ -384,7 +383,6 @@ error_reporting(E_ALL ^ E_NOTICE);
                   <a href="#" class="list-group-item list-group-item-action border-bottom">
                     <div class="row align-items-center">
                       <div class="col-auto">
-                        <!-- Avatar -->
                         <img alt="Image placeholder" src="ass/assets/img/team/profile-picture-2.jpg" class="avatar-md rounded">
                       </div>
                       <div class="col ps-0 ms-2">
@@ -410,7 +408,7 @@ error_reporting(E_ALL ^ E_NOTICE);
                   </a>
                 </div>
               </div>
-            </li>
+            </li> -->
             <li class="nav-item dropdown ms-lg-3">
               <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="media d-flex align-items-center">
